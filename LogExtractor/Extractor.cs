c94cadf59815ee70ae61141ea77c001bb0f59c61
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2022-01-11 18:16:32 +1100

    main: extract logs

--- LogExtractor/Extractor.cs
+++ LogExtractor/Extractor.cs
@@ -6,5 +6,14 @@ namespace LogExtractor
     public string[] topThreeMostVisitedUrls { get; set; } = { "None" };
     public string[] topThreeMostActiveIpAddresses { get; set; } = { "None" };
     public string[] ipLogs { get; set; } = { "None" };
+
+    /// <summary>
+    ///  Obtains the logs you want to extract the file from
+    /// </summary>
+    /// <param name="pathToFile">Path to the file where the logs are located</param>
+    public void GetLogs(string pathToFile)
+    {
+      ipLogs = File.ReadAllLines(pathToFile);
+    }
   }
 }
--- LogExtractor/Program.cs
+++ LogExtractor/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LogExtractor
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Count() == 0)
+      {
+        Console.WriteLine("No arguments found, needs one argument which is the path to the file.");
+        return;
+      }
+
+
+      Extractor extractor = new Extractor();
+
+      // Place logs into object
+      extractor.GetLogs(args[0]);
+    }
+  }
+}
